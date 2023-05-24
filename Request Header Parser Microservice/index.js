73c04998a27f22524911b04becf0e70fe1414c40
SzyJar <107247457+SzyJar@users.noreply.github.com>
2023-05-24 04:21:43 +0200

    Request Header Parser Microservice - Done

---
+++ Request Header Parser Microservice/index.js
@@ -0,0 +1,33 @@
+// index.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.ip; // Get the IP address of the client
+  const language = req.headers['accept-language']; // Get the preferred language of the client
+  const software = req.headers['user-agent']; // Get the user agent information of the client
+
+  res.json({ ipaddress, language, software });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
